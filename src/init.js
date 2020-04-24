b308ceeaf908fa32e910a1c0a4af19a8ff75a258
Rubenabdias1 <ruben_abdias@live.com>
2020-04-24 16:04:41 -0500

    Finished states

--- src/init.js
+++ src/init.js
@@ -1,4 +1,4 @@
-import game from "./gameState";
+import game, { handleUserAction } from "./gameState";
 import initButtons from "./buttons";
 import { TICK_RATE } from "./constants";
 
@@ -7,15 +7,12 @@ async function init() {
   initButtons(handleUserAction);
 
   let nextTimeToTick = Date.now();
-
   function nextAnimationFrame() {
-    const NOW = Date.now();
-
-    if (nextTimeToTick <= NOW) {
+    const now = Date.now();
+    if (nextTimeToTick <= now) {
       game.tick();
-      nextTimeToTick = NOW + TICK_RATE;
+      nextTimeToTick = now + TICK_RATE;
     }
-
     requestAnimationFrame(nextAnimationFrame);
   }
 
